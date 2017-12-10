7c3848ecb1a5f2441e1bcad236f3ec5aa2296c62
h5rcode <h5rcode@gmail.com>
2017-12-10 19:25:06 +0100

    Fixed walls rendering by setting the origin of the sprite correctly.

--- Shooter.Lib/Wall.cpp
+++ Shooter.Lib/Wall.cpp
@@ -13,6 +13,7 @@ Wall::Wall(Vector2 position, int width, int length, double orientation, std::str
 	_texture.setRepeated(true);
 
 	_sprite.setPosition(position.x, position.y);
+	_sprite.setOrigin(width / 2, length / 2);
 	_sprite.setRotation(_orientation);
 	_sprite.setTexture(_texture);
 }
